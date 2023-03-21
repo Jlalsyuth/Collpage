09478dfa64535402b189360e2f794a2823ee330c
Guspascad420 <Paacad@gmail.com>
2023-03-21 19:24:10 +0700

    Added navcontroller to welcome page, login route and sign up route

--- app/src/main/java/com/example/buddies/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/buddies/ui/navigation/AppNavGraph.kt
@@ -5,6 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.buddies.ui.screens.LoginPage
+import com.example.buddies.ui.screens.SignUpPage
 import com.example.buddies.ui.screens.WelcomePage
 
 @Composable
@@ -16,7 +18,13 @@ fun CollioApp(navController: NavHostController = rememberNavController()) {
 fun AppNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.WelcomePage.route) {
         composable(Screen.WelcomePage.route) {
-            WelcomePage()
+            WelcomePage(navController = navController)
+        }
+        composable(Screen.Login.route) {
+            LoginPage { navController.navigate(Screen.SignUp.route) }
+        }
+        composable(Screen.SignUp.route) {
+            SignUpPage()
         }
     }
 }
--- app/src/main/java/com/example/buddies/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/buddies/ui/navigation/Screen.kt
@@ -2,4 +2,6 @@ package com.example.buddies.ui.navigation
 
 sealed class Screen(val route: String) {
     object WelcomePage: Screen("welcome")
+    object Login: Screen("login")
+    object SignUp: Screen("SignUp")
 }

5abe0a8b090639c6d200816b6d25f36263a875b4
Guspascad420 <Paacad@gmail.com>
2023-03-21 07:51:09 +0700

    Added app main composable function

--- app/src/main/java/com/example/buddies/MainActivity.kt
+++ app/src/main/java/com/example/buddies/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.buddies.ui.navigation.CollioApp
 import com.example.buddies.ui.theme.BuddiesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CollioApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BuddiesTheme {
-        Greeting("Android")
     }
 }

8e427f82c78eadbf66621039f078d6a837f6d272
Guspascad420 <Paacad@gmail.com>
2023-03-21 19:25:49 +0700

    Added Sign up page implementations

---
+++ app/src/main/java/com/example/buddies/ui/screens/SignUpPage.kt
@@ -0,0 +1,40 @@
+package com.example.buddies.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.buddies.R
+import com.example.buddies.ui.theme.Poppins
+
+@Composable
+fun SignUpPage() {
+    Column {
+        Box {
+            Image(painterResource(R.drawable.vector_6), null, Modifier.padding(top = 22.dp))
+            Image(
+                painterResource(R.drawable.vector_3), null
+            )
+            Column(Modifier.padding(top = 45.dp, start = 35.dp)) {
+                Text(
+                    "Selamat Datang", fontFamily = Poppins,
+                    fontWeight = FontWeight.SemiBold, fontSize = 37.sp,
+                    color = Color(0xFFE5E8CD)
+                )
+                Text(
+                    "Silahkan buat akun baru!", fontFamily = Poppins,
+                    fontWeight = FontWeight.Light, fontSize = 20.sp,
+                    color = Color(0xFFE5E8CD)
+                )
+            }
+        }
+    }
+}

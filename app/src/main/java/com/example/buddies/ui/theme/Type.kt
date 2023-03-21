26b7ae782faf3943eaee533492c884ef8884ff54
Guspascad420 <Paacad@gmail.com>
2023-03-21 07:51:48 +0700

    Added font family

--- app/src/main/java/com/example/buddies/ui/theme/Type.kt
+++ app/src/main/java/com/example/buddies/ui/theme/Type.kt
@@ -2,9 +2,18 @@ package com.example.buddies.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.buddies.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(

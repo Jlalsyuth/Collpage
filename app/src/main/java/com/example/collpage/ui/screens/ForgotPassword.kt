4ea766439d7a82ecc6afe1140311b9176d954bde
Jlalsyuth <jlal.syuth@gmail.com>
2023-03-25 16:02:37 +0700

    added forgot Password UI

---
+++ app/src/main/java/com/example/collpage/ui/screens/ForgotPassword.kt
@@ -0,0 +1,40 @@
+package com.example.collpage.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.collpage.R
+import com.example.collpage.ui.theme.Poppins
+
+@Composable
+fun ForgotPassword() {
+    Column {
+        Box {
+            Image(painterResource(R.drawable.vector_3), null, Modifier.padding(top = 22.dp))
+            Image(
+                painterResource(R.drawable.vector_6), null
+            )
+            Column(Modifier.padding(top = 45.dp, start = 35.dp)) {
+                Text(
+                    "Lupa Password?", fontFamily = Poppins,
+                    fontWeight = FontWeight.SemiBold, fontSize = 37.sp,
+                    color = Color(0xFFE5E8CD)
+                )
+                Text(
+                    "Masukkan Email anda yang terdaftar pada Collpage!", fontFamily = Poppins,
+                    fontWeight = FontWeight.Light, fontSize = 20.sp,
+                    color = Color(0xFFE5E8CD)
+                )
+            }
+        }
+    }
+}

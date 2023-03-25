dd5db854441d8988ca166bdade5696c9fb091aed
Guspascad420 <Paacad@gmail.com>
2023-03-25 15:52:28 +0700

    Added home and second signup screen routes

--- app/src/main/java/com/example/collpage/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/collpage/ui/navigation/Screen.kt
@@ -4,4 +4,6 @@ sealed class Screen(val route: String) {
     object WelcomePage: Screen("welcome")
     object Login: Screen("login")
     object SignUp: Screen("SignUp")
+    object Home: Screen("home")
+    object SignUp2: Screen("SignUp2")
 }

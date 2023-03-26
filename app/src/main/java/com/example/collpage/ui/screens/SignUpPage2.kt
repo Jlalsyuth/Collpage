49763c5ec9be5713e36d3426aaec9f97830f2de0
Guspascad420 <Paacad@gmail.com>
2023-03-26 21:53:58 +0700

    Removed dependency

--- app/src/main/java/com/example/collpage/ui/screens/SignUpPage2.kt
+++ app/src/main/java/com/example/collpage/ui/screens/SignUpPage2.kt
@@ -1,7 +1,6 @@
 package com.example.collpage.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText

ef8ec7cd9ceb1fd6535211a14a4368200b8f5f63
Guspascad420 <Paacad@gmail.com>
2023-03-29 20:30:32 +0700

    Added method to get user attributes from database

--- app/src/main/java/com/example/collpage/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/collpage/ui/HomeViewModel.kt
@@ -2,12 +2,15 @@ package com.example.collpage.ui
 
 import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 private const val TAG = "MyActivity"
 
@@ -17,24 +20,79 @@ class HomeViewModel : ViewModel() {
     var selectedItem by mutableStateOf("")
     var user by mutableStateOf(User())
 
-    init {
-        getUserData()
+    private val _userProjects = mutableStateListOf<Project>()
+    val userProjects: List<Project> = _userProjects
+
+    private val _userEducations = mutableStateListOf<Education>()
+    val userEducations: List<Education> = _userEducations
+
+//    private val _userExperiences = mutableStateListOf<Experience>()
+//    val userEducations: List<Education> = _userEducations
+
+    fun getUserData() {
+        viewModelScope.launch {
+            db.collection("users").document(currentUserId.toString()).get()
+                .addOnCompleteListener {
+                    user = it.result.toObject(User::class.java)!!
+                    Log.d(TAG, user.toString())
+                }
+        }
     }
 
-    private fun getUserData() {
-        db.collection("users").document(currentUserId.toString()).get()
+    fun getUserEducations() {
+        viewModelScope.launch {
+            db.collection("educations")
+                .whereEqualTo("user_id", currentUserId.toString()).get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        _userEducations.add(document.toObject(Education::class.java))
+                    }
+                }
+        }
+    }
+
+    fun getUserProjects() {
+        viewModelScope.launch {
+            db.collection("projects_users")
+                .whereEqualTo("user_id", currentUserId.toString()).get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        getProject(document["project_id"].toString())
+                    }
+                }
+        }
+    }
+
+    private fun getProject(projectId: String) {
+        db.collection("projects").document(projectId).get()
             .addOnCompleteListener {
-                user = it.result.toObject(User::class.java)!!
-                Log.d(TAG, user.toString())
+                val project = it.result.toObject(Project::class.java)!!
+                _userProjects.add(project)
             }
     }
-
 }
 
 data class User(
     val email: String = "",
-    val name: String = "",
+    var name: String = "",
     val occupation: String? = "",
-    val profileDesc: String? = "",
-    val username: String? = ""
+    val profile_desc: String = "",
+    val username: String? = "",
+)
+
+data class Project(
+    val name: String = "",
+    val description: String = "",
+    val start_date: String = "",
+    val end_date: String = "",
+    val type: String = ""
+)
+
+data class Education(
+    val name: String = "",
+    val major: String = "",
+    val year_in: Int = 0,
+    val year_out: Int = 0,
+    val activities: List<String> = listOf(),
+    val user_id: String = ""
 )

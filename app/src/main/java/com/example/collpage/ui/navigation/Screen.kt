3309e523b57b7e054ccaf6b189736583ebc0a457
Guspascad420 <Paacad@gmail.com>
2023-03-29 20:32:04 +0700

    Added multiple app routes

--- app/src/main/java/com/example/collpage/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/collpage/ui/navigation/Screen.kt
@@ -3,9 +3,10 @@ package com.example.collpage.ui.navigation
 sealed class Screen(val route: String) {
     object WelcomePage: Screen("welcome")
     object Login: Screen("login")
-    object SignUp: Screen("SignUp")
+    object SignUp: Screen("signUp")
     object Home: Screen("home")
-    object SignUp2: Screen("SignUp2")
-    object ForgotPass: Screen("ForgotPass")
-    object EmailCheck: Screen("EmailCheck")
+    object SignUp2: Screen("signUp2")
+    object ForgotPass: Screen("forgotPass")
+    object EmailCheck: Screen("emailCheck")
+    object Profile: Screen("profile")
 }
